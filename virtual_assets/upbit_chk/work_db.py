73c0931ba6a2cef6b1987fbc7c14c9ac20c86058
wonsool <wsk2001@gmail.com>
2022-11-25 15:27:43 +0900

    update monitoring data
    update monitoring data

--- virtual_assets/upbit_chk/work_db.py
+++ virtual_assets/upbit_chk/work_db.py
@@ -358,7 +358,7 @@ def main():
     ##create_minute_table()
     #get_date('BTC', 30000, 'minute5')
 
-    work_date = '2022-11-22'
+    work_date = '2022-11-25'
     delete_db(work_date)
     insert_db(work_date)
 
