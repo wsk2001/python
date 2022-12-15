a251e496dc1b0fcf5e7426822e3d809f0865a464
wonsool <wsk2001@gmail.com>
2022-12-15 17:02:14 +0900

    Update mon.py

--- virtual_assets/upbit_chk/mon.py
+++ virtual_assets/upbit_chk/mon.py
@@ -231,7 +231,7 @@ def main(argv):
 
         print(f'fng: {fng}, earn: {mgn:.0f}, {pcnt:.2f}%,',
             f'BTC: ${btc_price:.2f} (${btc_rate:.2f}%),',
-            f'BTC(L20) 17773.23: ${eth_price:.5f} ({etc_rate:.2f}%)',
+            f'BTC(S10) 17718.04: ${eth_price:.5f} ({etc_rate:.2f}%)',
             f'cash, {int(cash):,d}, total {int(amt + cash):,d}')
 
         if 0 < item_count:
