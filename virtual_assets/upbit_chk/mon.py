7d80c0c3ddf32ca88be51d64491678ecb94cf22a
wonsool <wsk2001@gmail.com>
2023-06-02 16:54:03 +0900

    update minitoring data

--- virtual_assets/upbit_chk/mon.py
+++ virtual_assets/upbit_chk/mon.py
@@ -255,15 +255,11 @@ def main(argv):
         pcnt = (mgn / (amt + cash)) * 100.0
 
         btc_ohlcv = binance.fetch_ohlcv('BTC/USDT', timeframe='1d', limit=1)
-        eth_ohlcv = binance.fetch_ohlcv('ETH/USDT', timeframe='1d', limit=1)
         btc_price = btc_ohlcv[-1][4]
-        eth_price = eth_ohlcv[-1][4]
         btc_rate = ((btc_ohlcv[-1][4] / btc_ohlcv[-1][1]) - 1.0) * 100.0
-        etc_rate = ((eth_ohlcv[-1][4] / eth_ohlcv[-1][1]) - 1.0) * 100.0
 
         print(f'fng: {fng}, earn: {mgn:.0f}, {pcnt:.2f}%,',
             f'BTC: ${btc_price:.2f} (${btc_rate:.2f}%),',
-            f'ETH: ${eth_price:.5f} ({etc_rate:.2f}%)',
             f'cash, {int(cash):,d}, total {int(amt + cash):,d}')
 
         if 0 < item_count:
