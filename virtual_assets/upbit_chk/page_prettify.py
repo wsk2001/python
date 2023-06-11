93888eaed45be8c6124bbd837e27ccf5109cd679
wonsool <wsk2001@gmail.com>
2023-06-12 07:00:01 +0900

    update monitoring data

--- virtual_assets/upbit_chk/page_prettify.py
+++ virtual_assets/upbit_chk/page_prettify.py
@@ -41,6 +41,44 @@ async def get_dominance(site):
             break;
         return rtn_value
 
+
+async def get_aoa_position(site):
+    async with async_playwright() as pw:
+        browser = await pw.webkit.launch()
+        page = await browser.new_page()
+        await page.goto(site)
+        time.sleep(7)
+
+        html = await page.content()
+        soup = bs(html, 'html.parser').prettify()
+        lines = soup.splitlines()
+
+        await browser.close()
+
+        print(html)
+
+        list_str = []
+        list_str.clear()
+        fflag = False
+        icount = 0
+        rtn_value = ''
+
+        for line in lines:
+            line = line.strip()
+            if line.startswith('<'):
+                continue
+            if line.startswith('비트 코인 시장 지배력'):
+                fflag = True
+                continue
+            if fflag == False:
+                continue
+            print(line)
+            rtn_value = line
+            rtn_value = rtn_value.strip('%')
+            break;
+        return rtn_value
+
+
 async def get_data(site):
     async with async_playwright() as pw:
         # browser = await pw.chromium.launch()
@@ -169,11 +207,16 @@ async def main():
     #dominance = await get_dominance(coinness_schedule)
     #print(dominance)
 
+
+    leaderboard = 'https://btctools.io/kr/stats/leaderboard'
+    aoa_position = get_aoa_position(leaderboard)
+    print(aoa_position)
+
     #link = 'https://kimpga.com/statistics/liquidation'
     #await get_data(link)
 
-    kimpga_liquidation = 'https://kimpga.com/statistics/liquidation'
-    await get_kimpga_liquidation(kimpga_liquidation)
+    #kimpga_liquidation = 'https://kimpga.com/statistics/liquidation'
+    #await get_kimpga_liquidation(kimpga_liquidation)
 
 if __name__ == '__main__':
     asyncio.run(main())
